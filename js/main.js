d6a8addad71d3470646cd12bf6a95856eb0344ac
Lucas Oliveira <lucasdicasmt@gmail.com>
2020-05-10 15:41:03 -0300

    Alterações

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var nome = 'Lucas Oliveira'
+alert('Seja bem vindo '+nome);
+
+var lista = ['maça','pera','laranja'];
+lista.push('uva');
+console.log(lista);
+
+var d = new Date;
+alert(d.getMonth()+1);
